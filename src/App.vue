835b927de686ceb4bbe1056cc6af3510e3f9284e
Edenlw <568885816@qq.com>
2021-07-21 10:58:19 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <el-button>123</el-button>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
